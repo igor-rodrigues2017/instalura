0d5a980e16943bf47cc84674b61618308adbf5d1
Igor Rodrigues <igor.chagas.rodrigues@gmail.com>
2020-01-20 20:14:04 -0300

    add basic page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Timeline from "./components/Timeline";
+import './css/reset.css';
+import './css/login.css';
+import './css/timeline.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      <div className="main">
+          <Header />
+          <Timeline />
+      </div>
     </div>
   );
 }
