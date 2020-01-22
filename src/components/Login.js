e7d386ae4711181ca1217f823502b47f09362a3d
Igor Rodrigues <igor.chagas.rodrigues@gmail.com>
2020-01-21 23:42:25 -0300

    use ref to get input value and post information with fetch api

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,36 @@
 import React, {Component} from "react";
 
 export default class Login extends Component {
+	constructor() {
+		super();
+		this.state = {errorMsg: ''};
+	}
+
+	sendLogin(event) {
+		event.preventDefault();
+		fetch('http://localhost:8080/api/public/login', {
+			headers: {
+				'content-type': 'application/json'
+			},
+			method: 'Post',
+			body: JSON.stringify({login: this.login.value, senha: this.password.value})
+		}).then(response => {
+			if (!response.ok) {
+				this.setState({errorMsg:'login or password invalid'});
+			} else {
+				return response.text();
+			}
+		}).then(token => console.log(token));
+	}
+
 	render() {
 		return(
 			<div className="login-box">
 				<h1 className="header-logo">Instalura</h1>
-				<form>
-					<input type="text" />
-					<input type="password" />
+				<span>{this.state.errorMsg}</span>
+				<form onSubmit={this.sendLogin.bind(this)}>
+					<input type="text" ref={input => this.login = input} />
+					<input type="password" ref={input => this.password = input} />
 					<input type="submit" value="login" />
 				</form>
 			</div>
